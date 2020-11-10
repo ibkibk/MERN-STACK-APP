2b36949d31232d73cee22082be2d6a435f4decf5
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-10 17:13:28 +0000

    Route,   Redirect,   Switch, added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlaces";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/">
-        <Users />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 };
---
+++ src/places/pages/NewPlaces.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewPlaces = () => {
+    return (
+        <div>
+            <h2>Newplace works</h2>
+        </div>
+    )
+}
+export default NewPlaces;
