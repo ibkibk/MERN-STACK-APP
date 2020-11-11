c50c0e845c4fd660465666ca2feaf5c5b9c67d40
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-11 11:49:55 +0000

    new components added

---
+++ src/users/components/UserItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserItem from "./UsersItem.css"
+
+const UsersItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default UsersItem;
---
+++ src/users/components/UsersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserList from "./UsersList.css"
+
+const UsersList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default UsersList;
