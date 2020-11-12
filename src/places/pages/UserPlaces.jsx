ec12b9fd6fb3e270fd757131ee065e0b02201b85
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-12 11:38:35 +0000

    olacelist and placeitem added

---
+++ src/places/pages/UserPlaces.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PlaceList from "../components/PlaceList"
+const UserPlaces = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default  UserPlaces;
