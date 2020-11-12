6739daa3f2fbd33203a5a1ec2d1b3ecbaf5fc487
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-12 15:42:34 +0000

    validation title add

--- src/places/pages/NewPlaces.jsx
+++ src/places/pages/NewPlaces.jsx
@@ -4,7 +4,7 @@ import Input from "../../shared/Component/FormElement/Input"
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title"/>
     </form>
   );
 };
