f8869aea1f4e20741a1730f60c44a187b4563b66
Ibrahim Kurhan <ikurhan@outlook.com>
2020-11-12 17:36:01 +0000

    usecONTEXT HOOK ADDED

--- src/shared/Component/Navigation/NavLinks.jsx
+++ src/shared/Component/Navigation/NavLinks.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import "./NavLinks.css"
 import { NavLink } from "react-router-dom";
+import {AuthContext} from "../../context/auth-context"
 
-export default function NavLinks() {
+
+const NavLinks = () => {
+
+    const auth = useContext(AuthContext)
+
     return (
        <ul className="nav-links">
           <li>
               <NavLink to="/" exact>ALL USERS</NavLink>
           </li>
+          {auth.isLoggedIn && (        
           <li>
               <NavLink to="/u1/places">MY PLACES</NavLink>
           </li>
+          )}
+            {auth.isLoggedIn && (
           <li>
               <NavLink to="/places/new">ADD PLACE</NavLink>
-          </li>
+          </li> 
+          )}       
+
+            {!auth.isLoggedIn && (
           <li>
               <NavLink to="/auth">AUTHENTICATE</NavLink>
           </li>
+          )}
+
+         
        </ul>
     )
 }
+export default NavLinks;
